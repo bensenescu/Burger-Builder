a329589347fd1c36a648746828853594784ea52d
senescub <senescu.b@husky.neu.edu>
2018-12-27 19:06:02 -0500

    Burger Builder Finished

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button';
+
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
-            return <li>
+            return <li key={igKey}>
                 <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
             </li>;
         }
@@ -17,6 +19,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to checkout</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
         </Aux>
     )
 };
